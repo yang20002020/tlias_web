98881c4740e01554866ab949484d9552f5d936c9
yang20002020 <2628636987@qq.com>
2023-10-16 00:27:22 +0800

    登录校验---interceptor---详解

---
+++ tlias-web-management_017/src/main/java/com/itheima/interceptor/LoginCheckInterceptor.java
@@ -0,0 +1,25 @@
+package com.itheima.interceptor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class LoginCheckInterceptor implements HandlerInterceptor {
+    @Override //目标资源方法运行前运行, 返回true: 放行, 放回false, 不放行
+    public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
+        System.out.println("preHandle ...");
+        return true;
+    }
+
+    @Override //目标资源方法运行后运行
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        System.out.println("postHandle ...");
+    }
+
+    @Override //视图渲染完毕后运行, 最后运行
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("afterCompletion...");
+    }
+}
