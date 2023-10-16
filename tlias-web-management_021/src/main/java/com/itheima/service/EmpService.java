979fb5d22ffa4d27731cd7a5d512c26cffe85f9a
yang20002020 <2628636987@qq.com>
2023-10-16 17:24:11 +0800

    根据部门id删除部门数据

---
+++ tlias-web-management_021/src/main/java/com/itheima/service/EmpService.java
@@ -0,0 +1,46 @@
+package com.itheima.service;
+
+import com.itheima.pojo.Emp;
+import com.itheima.pojo.PageBean;
+
+import java.time.LocalDate;
+import java.util.List;
+
+/**
+ * 员工管理
+ */
+public interface EmpService {
+    /**
+     * 分页查询
+     * @param page
+     * @param pageSize
+     * @return
+     */
+    PageBean page(Integer page, Integer pageSize,String name, Short gender,LocalDate begin,LocalDate end);
+
+    /**
+     *  分页查询
+     * @param page
+     * @param pageSize
+     * @return
+     */
+    PageBean page(Integer page, Integer pageSize);
+
+    /**
+     * 批量删除
+     * @param ids
+     */
+    void delete(List<Integer> ids);
+
+    void save(Emp emp);
+
+
+    void update(Emp emp);
+
+    /**
+     * 员工登录
+     * @param emp
+     * @return
+     */
+    Emp login(Emp emp);
+}
---
+++ tlias-web-management_021/src/main/java/com/itheima/service/impl/DeptServiceImpl.java
@@ -0,0 +1,40 @@
+package com.itheima.service.impl;
+
+import com.itheima.mapper.DeptMapper;
+import com.itheima.mapper.EmpMapper;
+import com.itheima.pojo.Dept;
+import com.itheima.service.DeptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class DeptServiceImpl implements DeptService {
+
+    @Autowired
+    private DeptMapper deptMapper;
+
+    @Autowired
+    private EmpMapper empMapper;
+
+    @Override
+    public List<Dept> list() {
+        return deptMapper.list();
+    }
+
+    @Override
+    public void delete(Integer id) {
+        deptMapper.deleteById(id); //根据部门id删除部门数据
+        empMapper.deleteByDeptId(id);//根据部门id删除部门下的员工
+    }
+
+    @Override
+    public void add(Dept dept) {
+        dept.setCreateTime(LocalDateTime.now());
+        dept.setUpdateTime(LocalDateTime.now());
+
+        deptMapper.insert(dept);
+    }
+}
