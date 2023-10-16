f76da98af4900226f6299e428df82149fe47ee45
yang20002020 <2628636987@qq.com>
2023-10-16 23:36:08 +0800

    事务管理  运行时异常

---
+++ tlias-web-management_023/src/main/java/com/itheima/service/impl/DeptServiceImpl.java
@@ -0,0 +1,46 @@
+package com.itheima.service.impl;
+
+import com.itheima.mapper.DeptMapper;
+import com.itheima.mapper.EmpMapper;
+import com.itheima.pojo.Dept;
+import com.itheima.service.DeptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class DeptServiceImpl implements DeptService {
+
+    @Autowired
+    private DeptMapper deptMapper;
+
+    @Autowired
+    private EmpMapper empMapper;
+
+    @Override
+    public List<Dept> list() {
+        return deptMapper.list();
+    }
+
+    @Transactional  //spring事务管理
+    @Override
+    public void delete(Integer id) throws Exception {
+        deptMapper.deleteById(id); //根据部门id删除部门数据
+
+        if(true){
+            throw new Exception("出错了.....");
+        }
+        empMapper.deleteByDeptId(id);//根据部门id删除部门下的员工
+    }
+
+    @Override
+    public void add(Dept dept) {
+        dept.setCreateTime(LocalDateTime.now());
+        dept.setUpdateTime(LocalDateTime.now());
+
+        deptMapper.insert(dept);
+    }
+}
