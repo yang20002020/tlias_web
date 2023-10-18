c2d0a097c49a2046f20a736980644d433e89bac1
yang20002020 <2628636987@qq.com>
2023-10-19 00:00:05 +0800

    AOP 连接点

---
+++ springboot-aop-quickstart_07/src/main/java/com/itheima/SpringbootAopQuickstartApplication.java
@@ -0,0 +1,12 @@
+package com.itheima;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAopQuickstartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAopQuickstartApplication.class, args);
+    }
+
+}
