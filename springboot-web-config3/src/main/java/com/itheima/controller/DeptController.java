e6b98a6c68e94a080a1e1ca55fb932d50d48133b
yang20002020 <2628636987@qq.com>
2023-10-19 19:05:52 +0800

    @Lazy

---
+++ springboot-web-config3/src/main/java/com/itheima/controller/DeptController.java
@@ -0,0 +1,39 @@
+package com.itheima.controller;
+
+import com.itheima.pojo.Dept;
+import com.itheima.pojo.Result;
+import com.itheima.service.DeptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/depts")
+public class DeptController {
+
+    @Autowired
+    private DeptService deptService;
+
+    public DeptController(){
+        System.out.println("DeptController constructor ....");
+    }
+
+    @GetMapping
+    public Result list(){
+        List<Dept> deptList = deptService.list();
+        return Result.success(deptList);
+    }
+
+    @DeleteMapping("/{id}")
+    public Result delete(@PathVariable Integer id)  {
+        deptService.delete(id);
+        return Result.success();
+    }
+
+    @PostMapping
+    public Result save(@RequestBody Dept dept){
+        deptService.save(dept);
+        return Result.success();
+    }
+}
